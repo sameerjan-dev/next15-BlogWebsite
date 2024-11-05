8d65498962f46254a9a11493daa8d155736cc128
Sameejan1 <sameerjay1996@gmail.com>
2024-11-05 11:54:15 +0500

    added links/navbar responsiveness and changes in layout

--- next15-blog-app/src/components/navbar/links/Links.jsx
+++ next15-blog-app/src/components/navbar/links/Links.jsx
@@ -1,10 +1,10 @@
-import Link from 'next/link'
+
 import React from 'react'
 import styles from "./links.module.css"
 import NavLink from './navLink/navLink'
-export default function Links() {
 
-    const Links = [
+
+ const Links = [
         {
         title : "HomePage",
         path:"/"
@@ -19,12 +19,40 @@ export default function Links() {
         title : "Blog",
         path:"/blog"
     }
-]
-  return (
+    ]
+export default function Links() {
+ const [open, setOpen] = useState(false);
+   
+    let session = true;
+    let isAdmin = true;
+
+    return (
+       <div className={styles.container}>
     <div className={styles.links}>
           {Links.map((link) => (
               <NavLink link={link} key={link.title}>{link.title}</NavLink>
+          ))}{
+              session ? (
+                  <>{
+                      isAdmin && (
+                          <NavLink link={{ title: "Admin", path: "/admin" }} />
+                      )}
+                      <button className={styles.logout}>Logout</button>
+                  </>
+              ) : (
+                      <NavLink link={{title: "Login",path :"/login"}} />
+          
+              )
+          }
+            </div>
+            <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>{
+                 open && (
+        <div className={styles.mobileLinks}>
+          {Links.map((link) => (
+            <NavLink item={link} key={link.title} />
           ))}
-    </div>
+        </div>
+      )}
+            </div>
   )
 }
