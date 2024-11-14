33a90b68f582caa9c01a257431db96222b5cbd1b
Sameejan1 <136233911+sameerjan1@users.noreply.github.com>
2024-11-14 15:03:07 +0500

    Update page.jsx

--- next15-blog-app/src/app/blog/[slug]/page.jsx
+++ next15-blog-app/src/app/blog/[slug]/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
 export default function SinglePostPage() {
+  const { slug } = params;
+
   return (
-    <div>
-      Single Post Page
+    <div className={styles.container}>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+          </div>
+        </div>
+        <div className={styles.content}>{post.desc}</div>
+      </div>
     </div>
-  )
+  );
 }
